2b5fc4ba0d1a4a4f1180a8d3aaf28bcabd2c396e
Sergey Zveroboev <szveroboev@gmail.com>
2018-01-05 20:14:41 +0300

    error handling

--- github.js
+++ github.js
@@ -1,6 +1,8 @@
 const https = require('https');
 
 const getRepos = (userName, done) => {
+  if (!userName) return done(new Error('Необходимо указать имя пользователя'));
+
   const options = {
     hostname: 'api.github.com',
     path: `/users/${userName}/repos`,
@@ -8,14 +10,29 @@ const getRepos = (userName, done) => {
       'User-Agent': 'Node.JS-GitHub'
     }
   };
-  https.get(options, res => {
+
+  const req = https.get(options, res => {
     let body = '';
-    res.setEncoding('utf-8');
-    res.on('data', data => body += data);
-    res.on('end', () => {
-      done(null, JSON.parse(body));
-    });
+
+    if (res.statusCode === 200) {
+      res.setEncoding('utf-8');
+      res.on('data', data => body += data);
+      res.on('end', () => {
+        try {
+          const result = JSON.parse(body);
+          done(null, result);
+        } catch (error) {
+          done(new Error(`Не удалось обработать данные (${error.message})`));
+        }
+
+      });
+    } else {
+      done(new Error(`Не удалось получить данные от сервера (${res.statusCode} ${res.statusMessage})`));
+    }
+
   });
+
+  req.on('error', error => done(new Error(`Не удалось отправить запрос (${error.message})`)));
 };
 
 module.exports = {
